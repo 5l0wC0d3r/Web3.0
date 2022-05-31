aaf7ae7a08632520ec13cf3a869a5bf3e1804705
5l0wC0d3r <105980346+5l0wC0d3r@users.noreply.github.com>
2022-05-31 17:32:57 +0530

    Update Assignment1.py

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives import hashes
 import time
 
 
-def hasher(istring, inum):
+def hasher(istring, inum):  # hashing function to calculate the hex
     istring = bytes(istring + str(inum), "utf-8")
     digest = hashes.Hash(hashes.SHA256())
     digest.update(istring)
@@ -13,11 +13,10 @@ def hasher(istring, inum):
 
 string = input("Enter string to be hashed:")
 num = 0
-target = int("0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 0)
+target = int("0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 0)  # target value given in the task
 start = time.time()
 while True:
-    # print(hasher(string, num))
-    if hasher(string, num) <= target:
+    if hasher(string, num) <= target:  # checking each nonce  value
         end = time.time()
         print("The nonce value for the  target: ",string + str(num))
         print("The time taken to find the nonce value: ",end - start)
